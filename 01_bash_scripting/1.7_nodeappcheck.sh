b90ea54d8fcd7061547c6e30580b3b8cdf805dbc
vadzim <vadzimk@hotmail.com>
2022-01-16 02:06:15 -0800

    1.7

---
+++ 01_bash_scripting/1.7_nodeappcheck.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if ! [ $(node --version) ]
+then
+	apt -y install nodejs
+	version=$(node --version)
+	echo "installed node version $version"
+fi
+
+if ! [ netstat ]
+then
+	apt -y install net-tools
+fi
+
+
+if ! [ $(npm --verison) ]
+then
+	apt -y install npm
+	version=$(npm --version)
+	echo "installed npm version $version"
+fi
+
+if ! [ -d "app" ]
+then
+	mkdir app
+fi
+
+if ! [ -f "./app/bootcamp-node-envvars-project-1.0.0.tgz" ]
+then
+	wget -P ./app https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
+fi
+
+unzipped=$(tar -xvf ./app/bootcamp-node-envvars-project-1.0.0.tgz -C ./app)
+echo -e "unzipped files:\n$unzipped"
+
+export APP_ENV=dev
+export DB_USER=myuser
+export DB_PWD=mysecret
+
+cd ./app/package
+
+npm install
+
+node server.js &
+
+application=$(netstat -patun | grep "3000")
+
+
+	pid=$(echo "$application" | awk '{print $7}')
+	echo "port 3000 busy by pid $pid"
+
+	application=$(netstat -patun | grep node)
+	echo -e "application running\n$application"
